e161a1457229d8fcfa4d7ad8b1dae39d19a0f876
McMenemy <mr_mcmenemy@yahoo.com>
2016-11-04 11:18:15 -0700

    change code for a simple docker test

---
+++ goWeb.go
@@ -0,0 +1,41 @@
+/* package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+} */
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
+
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
+}
